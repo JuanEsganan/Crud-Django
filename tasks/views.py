184ecb202cef62d4675eab7e2e50eb3491c6aba2
juan ganan <j_aness@hotmail.com>
2023-06-05 01:47:05 -0500

    completing the view sign_in

--- tasks/views.py
+++ tasks/views.py
@@ -43,14 +43,19 @@ def sign_out (request):
     return redirect ("home")
 
 def sign_user(request):
+    #only showing the form
     if request.method == "GET":
         return render (request, "signin.html", {
             "form": AuthenticationForm
         })
     else:
+        #checks if the user exists
         user = authenticate (request, username=request.POST["username"], password=request.POST["password"])
         if user is None:
             return render (request, "signin.html", {
             "form": AuthenticationForm,
             "error": "Username or password is incorrect"
             })
+        else:
+            login (request, user)
+            return redirect ("tasks")
