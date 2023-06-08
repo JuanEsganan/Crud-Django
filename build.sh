ca9498679614864d7aa0094cddb5a6e09380119b
JuanEsganan <j_aness@hotmail.com>
2023-06-08 04:43:17 -0500

    updated build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 set -o errexit
 
 #poetry install
-#pip install -r requirements.txt
-pip install --upgrade pip
+pip install -r requirements.txt
+#pip install --upgrade pip
 
 python manage.py collectstatic --no-input
 python manage.py migrate
