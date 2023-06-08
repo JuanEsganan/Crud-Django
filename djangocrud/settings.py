c5e70bf36bcd80ba1623c7a0375b878e9a9b5099
JuanEsganan <j_aness@hotmail.com>
2023-06-08 04:30:27 -0500

    requirements updated

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -80,15 +80,13 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': { dj_database_url.config(
+    'default': dj_database_url.config(
         default="postgresql://postfres:postgres@localhost/postgres",
         conn_max_age=600
     )
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': BASE_DIR / 'db.sqlite3',
-        
-    }
-}
+        }
 
 
 # Password validation
